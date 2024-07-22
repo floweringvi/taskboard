137aafb9de68d9e350d8c6268f5510139f96bb8c
Violet Stanziani <stanzianiviolet@gmail.com>
2024-07-21 20:00:17 -0400

    functional task board finishing drop

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -33,7 +33,9 @@ function createTaskCard(task) {
      taskCardHeader = $('<h2>')
      taskCardsubHeader =$('<h3>')
      taskCardBody = $('<p>')
-
+     taskCardDelete = $('<button>')
+     taskCardDelete.addClass("deleteBtn")
+     
      taskCardHeader.text(taskHeader)
      taskCardsubHeader.text(taskDueDate)
      taskCardBody.text(taskDueDate)
@@ -76,9 +78,7 @@ event.preventDefault();
 
 }
 
-AddTaskBtn = $('.btn-primary')
 
-AddTaskBtn.on('click',handleAddTask())
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
@@ -92,6 +92,8 @@ for(let i =0; i < taskList.length;i++){
 }
 
 
+
+
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
 
@@ -99,15 +101,28 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+//render tasks and create cards from those rendered tasks
+renderTaskList();
+//add task to taskList by pressing button
+AddTaskBtn = $('.btn-primary')
 
-});
+AddTaskBtn.on('submit',handleAddTask())
+
+//delete button to run function to delete based on matching id
+
+Deletebtn = $('.deleteBtn')
+
+Deletebtn.on('click', handleDeleteTask)
 
 //datepicker function
-
-
 $(function () {
    $('#datepicker').datepicker({
      changeMonth: true,
      changeYear: true,
    });
  });
+});
+
+
+
+
